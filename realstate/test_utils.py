cba5aedf383e60f54fab901c5dbfd54043c8bee0
Ismael <ismael.jimenezsanchez@telefonica.com>
2023-04-16 13:45:47 +0200

    fix expected value

--- realstate/test_utils.py
+++ realstate/test_utils.py
@@ -3,4 +3,4 @@ from utils import compute_anual_cost
 
 def test_compute_anual_cost():
     anual_cost = compute_anual_cost(170000, 10)
-    assert anual_cost == 170000
+    assert anual_cost == 170000.0
